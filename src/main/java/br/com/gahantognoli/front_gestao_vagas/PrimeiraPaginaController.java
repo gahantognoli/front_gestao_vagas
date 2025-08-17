fd01e6deec47e5f64fb24702467b27b50d74aa19
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-08-17 16:30:21 -0300

    add login and first page HTML templates with corresponding controller

---
+++ src/main/java/br/com/gahantognoli/front_gestao_vagas/PrimeiraPaginaController.java
@@ -0,0 +1,20 @@
+package br.com.gahantognoli.front_gestao_vagas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PrimeiraPaginaController {
+
+    @GetMapping("/home")
+    public String primeiraPagina() {
+        return "primeiraPagina";
+    }
+
+    @GetMapping("/login")
+    public String loginCandidato() {
+        return "candidate/login";
+    }
+}
